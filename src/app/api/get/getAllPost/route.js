63851c621098172ec4c21cb1031df4520260e105
murad <vvanhellsing30@gmail.com>
2025-05-10 14:43:06 +0600

    home section changes

--- src/app/api/get/getAllPost/route.js
+++ src/app/api/get/getAllPost/route.js
@@ -5,6 +5,8 @@ import connectDb from "../../../../../utils/connectDb";
 export const GET = async() =>{
     await connectDb();
     try {
+        // const {location} =await params;
+        // console.log("recieved location", location);
         const allPost = await Post.find().sort({createdAt: -1});
         return NextResponse.json({allPost}, {status: 200});
     } catch (error) {
--- src/hooks/useGetAllPost.jsx
+++ src/hooks/useGetAllPost.jsx
@@ -1,13 +1,20 @@
+"use client";
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
+// import useGetCurrentLocation from './useGetCurrentLocation';
 
 const useGetAllPost = () => {
-    const {data: allPost, isLoading, refetch: refetchVotesAndBoycott} = useQuery({
-        queryKey: ['allPost'],
+    // const {location} = useGetCurrentLocation();
+    // console.log(location);
+    const {data: allPost = [], isLoading, refetch: refetchVotesAndBoycott} = useQuery({
+        queryKey: ['location', location],
         queryFn: async() =>{
-            const res = await axios.get('/api/get/getAllPost');
+            // if(!location) return [];
+            // console.log(location)
+            const res = await axios.get(`/api/get/getAllPost`);
+            // console.log("getall post", res.data.allPost)
             return res.data.allPost;
-        }
+        },
     })
     return {allPost, isLoading, refetchVotesAndBoycott};
 };
